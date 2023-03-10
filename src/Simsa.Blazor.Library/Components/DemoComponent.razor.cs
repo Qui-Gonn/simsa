98d401215cff9af9d96e1485e57221bae27a1bde
Qui-Gonn Jinn <Qui-Gonn.Jinn@gmx.net>
2023-03-10 11:04:43 +0100

    first draft of test form and dynamic test form

--- src/Simsa.Blazor.Library/Components/DemoComponent.razor.cs
+++ src/Simsa.Blazor.Library/Components/DemoComponent.razor.cs
@@ -1,5 +1,39 @@
 ﻿namespace Simsa.Blazor.Library.Components;
 
+using System.ComponentModel.DataAnnotations;
+
 public partial class DemoComponent
 {
+    private Person Person => new ("Michael Binder", new DateOnly(1989, 2, 8), Gender.Male);
+}
+
+public class Person
+{
+    public Person(string name, DateOnly dateOfBirth, Gender gender)
+    {
+        this.Name = name;
+        this.DateOfBirth = dateOfBirth;
+        this.Gender = gender;
+    }
+
+    [DataType(DataType.Date)]
+    [Display(Name = "Date of Birth")]
+    public DateOnly DateOfBirth { get; init; }
+
+    public Gender Gender { get; init; }
+
+    [DataType(DataType.Text)]
+    [Display(Name = "Name")]
+    public string Name { get; init; }
+}
+
+public enum Gender
+{
+    Unknown = 0,
+
+    Female = 1,
+
+    Male = 2,
+
+    Other = 3
 }
