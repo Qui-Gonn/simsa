146240ad325060b5f3d4faa9326b53963ec2cc17
Qui-Gonn <Qui-Gonn.Jinn@gmx.net>
2023-12-01 15:52:14 +0100

    restructure app

--- src/Simsa.Blazor.Client/Program.cs
+++ src/Simsa.Blazor.Client/Program.cs
@@ -1,5 +1,11 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+using Simsa.Blazor.Client;
+using Simsa.Blazor.Library.Extensions;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+await builder.ConfigureLicense();
+builder.Services.AddSimsaFrontEndServices(builder.Configuration);
 
 await builder.Build().RunAsync();
--- src/Simsa.Blazor.Library/Extensions/ServiceCollectionExtension.cs
+++ src/Simsa.Blazor.Library/Extensions/ServiceCollectionExtension.cs
@@ -8,9 +8,11 @@ using Syncfusion.Licensing;
 
 public static class ServiceCollectionExtension
 {
+    public const string SyncfusionLicenseKey = "SyncfusionLicense";
+
     public static IServiceCollection AddSimsaFrontEndServices(this IServiceCollection services, IConfiguration configuration)
     {
-        SyncfusionLicenseProvider.RegisterLicense(configuration["SyncfusionLicense"]);
+        SyncfusionLicenseProvider.RegisterLicense(configuration[SyncfusionLicenseKey]);
         services.AddSyncfusionBlazor();
 
         return services;
