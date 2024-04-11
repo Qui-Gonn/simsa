5672e2cf5dbfe4873d163db17b831d31627b31da
Qui-Gonn <Qui-Gonn.Jinn@gmx.net>
2024-04-11 19:54:02 +0200

    add new projects

---
+++ src/Simsa.Client/Extensions/WebAssemblyHostBuilderExtension.cs
@@ -0,0 +1,20 @@
+﻿namespace Simsa.Client.Extensions;
+
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+public static class WebAssemblyHostBuilderExtension
+{
+    public static async Task ConfigureLicense(this WebAssemblyHostBuilder builder)
+    {
+        var httpClient = new HttpClient
+        {
+            BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+        };
+
+        using var response = await httpClient.GetAsync("license");
+        if (response.IsSuccessStatusCode)
+        {
+            builder.Configuration[ServiceCollectionExtension.SyncfusionLicenseKey] = await response.Content.ReadAsStringAsync();
+        }
+    }
+}
---
+++ src/Simsa/Program.cs
@@ -0,0 +1,39 @@
+using Simsa.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+////builder.Configuration.AddJsonFile("license.json");
+////builder.Services.AddSimsaFrontEndServices(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Simsa.Client._Imports).Assembly);
+
+////app.MapGet("license", () => builder.Configuration[ServiceCollectionExtension.SyncfusionLicenseKey]);
+
+app.Run();
